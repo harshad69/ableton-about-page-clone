779f396dbe02238e48fae23b431309dfff2b13fc
Harshad <68157712+harshad69@users.noreply.github.com>
2022-06-09 19:54:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,38 @@ const sectionDiv = document.getElementById('section-div')
 
 function showDiv() {
  if (sectionDiv.classList.contains('display-none')) {
-  sectionDiv.classList.remove('display-none')
-  more.innerHTML = "More -"
+  sectionDiv.classList.remove('display-none');
+  more.innerHTML = "More -";
  }
  else {
-  sectionDiv.classList.add('display-none')
-  more.innerHTML = "More +"
+  sectionDiv.classList.add('display-none');
+  more.innerHTML = "More +";
  }
 }
 
 more.addEventListener('click', showDiv);
+
+const menuExpand = document.getElementById('menu-expand');
+const menuCollapse = document.getElementById('menu-collapse');
+const toExpand = document.getElementById('expand');
+const navbar2 = document.getElementById('navbar-2');
+const navbar = document.getElementById('navbar');
+
+function menuShow() {
+ if (toExpand.classList.contains('display-none')) {
+  toExpand.classList.remove('display-none');
+  menuCollapse.innerHTML = '<p style="padding-top: 1.2rem; font-size: 1.1rem; font-weight: 400;">Menu <img src="images/icons8-collapse-arrow-24-white.png" alt="collapse-arrow"></p>';
+  navbar2.classList.add('display-none');
+  navbar.classList.add('display-none');
+ }
+ else {
+  toExpand.classList.add('display-none');
+  menuExpand.innerHTML = '<p>Menu <img src="images/icons8-expand-arrow-24.png" alt="expand-arrow"></p>'
+  navbar2.classList.remove('display-none');
+  navbar.classList.remove('display-none');
+
+ }
+}
+
+menuExpand.addEventListener('click', menuShow);
+menuCollapse.addEventListener('click', menuShow);
